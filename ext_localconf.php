4e0ee23ce95fbd20ab5af021a063419733b61603
Thomas Löffler <loeffler@spooner-web.de>
2022-01-24 16:17:18 +0100

    [TASK] Use TYPO3 constant for v11 support

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or defined('TYPO3_MODE') or die('Access denied.');
 
 if (TYPO3_MODE === 'FE') {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterSubmit'][1517255631] = \Extrameile\FormDynamicRecipient\Hooks\FormElementsOnSubmitHooks::class;
