e3ddffde84ec96697f7e962523a867fc268a7989
Andreas Kießling <kiessling@extrameile-gehen.de>
2022-01-24 21:19:54 +0100

    remove usage of deprecated TYPO3_MODE constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,6 @@
 
 defined('TYPO3') or defined('TYPO3_MODE') or die('Access denied.');
 
-if (TYPO3_MODE === 'FE') {
+call_user_func(static function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterSubmit'][1517255631] = \Extrameile\FormDynamicRecipient\Hooks\FormElementsOnSubmitHooks::class;
-}
+});
