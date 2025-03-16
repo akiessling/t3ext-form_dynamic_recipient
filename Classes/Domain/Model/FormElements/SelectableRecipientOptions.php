83e18308c5a4c6440b456cbacd8a1da02c1fa822
Andreas Kießling <kontakt@kiessling.tech>
2025-03-16 19:26:43 +0100

    update cacheTag handling, use frontend.cache.collector

--- Classes/Domain/Model/FormElements/SelectableRecipientOptions.php
+++ Classes/Domain/Model/FormElements/SelectableRecipientOptions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AndreasKiessling\FormDynamicRecipient\Domain\Model\FormElements;
 
 use AndreasKiessling\FormDynamicRecipient\Domain\Repository\RecipientRepository;
+use TYPO3\CMS\Core\Cache\CacheTag;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class SelectableRecipientOptions extends \TYPO3\CMS\Form\Domain\Model\FormElements\GenericFormElement
@@ -20,14 +21,15 @@ class SelectableRecipientOptions extends \TYPO3\CMS\Form\Domain\Model\FormElemen
             $value = (int) $value;
             // use uid of current page if pageUid is not set
             if ($value === 0) {
-                $pageArguments = $GLOBALS['TYPO3_REQUEST']->getAttribute('routing');
+                $pageArguments = $this->getRequest()->getAttribute('routing');
                 $value = $pageArguments->getPageId();
             }
             $this->setProperty('options', $this->getOptions($value));
+
             // automatic cache clearing with data handler, if anything in that page changes
-            if ($GLOBALS['TSFE'] instanceof \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController) {
-                $GLOBALS['TSFE']->addCacheTags(['pageId_' . $value]);
-            }
+            $cacheDataCollector = $this->getRequest()->getAttribute('frontend.cache.collector');
+            $cacheDataCollector->addCacheTags(new CacheTag('pageId_' . $value));
+
             return;
         }
 
