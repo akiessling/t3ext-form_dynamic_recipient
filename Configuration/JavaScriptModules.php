f329aee9caed144bffb6553476da7340b50e52a1
Andreas Kießling <kontakt@kiessling.tech>
2025-03-16 18:44:46 +0100

    Add support for TYPO3 v13

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dependencies' => ['form'],
+    'imports' => [
+        '@extrameile/form-dynamic-recipient/' => 'EXT:form_dynamic_recipient/Resources/Public/JavaScript/',
+    ],
+];
--- Resources/Public/JavaScript/Backend/FormEditor/ViewModel.js
+++ Resources/Public/JavaScript/Backend/FormEditor/ViewModel.js
@@ -1,130 +1,17 @@
-"use strict";
 /**
- * Module: TYPO3/CMS/FormElementFormDynamicRecipient/Backend/FormEditor/ViewModel
+ * Module: @extrameile/form-dynamic-recipient/Backend/FormEditor/ViewModel.js
  */
+import * as Helper from '@typo3/form/backend/form-editor/helper.js';
 
-define(['jquery', 'TYPO3/CMS/Form/Backend/FormEditor/StageComponent'], function ($, StageComponent) {
+export function bootstrap(formEditorApp) {
+    Helper.bootstrap(formEditorApp);
 
-    return (function ($, StageComponent) {
-
-        /**
-         * @private
-         *
-         * @var object
-         */
-        var _formEditorApp = null;
-
-        /**
-         * @private
-         *
-         * @return object
-         */
-        function getFormEditorApp() {
-            return _formEditorApp;
-        }
-
-        /**
-         * @private
-         *
-         * @return object
-         */
-        function getPublisherSubscriber() {
-            return getFormEditorApp().getPublisherSubscriber();
-        }
-
-        /**
-         * @private
-         *
-         * @return void
-         */
-        function _subscribeEvents() {
-
-            /**
-             * @private
-             *
-             * @param string
-             * @param array
-             *              args[0] = formElement
-             *              args[1] = template
-             * @return void
-             * @subscribe view/stage/abstract/render/template/perform
-             */
-            getPublisherSubscriber().subscribe('view/stage/abstract/render/template/perform', function (topic, args) {
-                if (args[0].get('type') === 'FormDynamicRecipient') {
-                    StageComponent.renderSelectTemplates(args[0], args[1]);
-                }
-            });
-        }
-
-        function getDynamicRecipientAliasValues() {
-            var nonCompositeNonToplevelFormElements = getFormEditorApp().getNonCompositeNonToplevelFormElements();
-
-            var allowedDynamicProperties = [];
-
-            for (var i = 0, len = nonCompositeNonToplevelFormElements.length; i < len; ++i) {
-                var nonCompositeNonToplevelFormElement;
-
-                nonCompositeNonToplevelFormElement = nonCompositeNonToplevelFormElements[i];
-
-                if (nonCompositeNonToplevelFormElement.get('type') === 'FormDynamicRecipient') {
-                    var asValue = nonCompositeNonToplevelFormElement.get('properties.assignedVariable');
-                    allowedDynamicProperties.push(asValue);
-                }
+    formEditorApp.getPublisherSubscriber().subscribe(
+        'view/stage/abstract/render/template/perform',
+        (topic, args) => {
+            if (args[0].get('type') === 'FormDynamicRecipient') {
+                formEditorApp.getViewModel().getStage().renderSimpleTemplateWithValidators(args[0], args[1]);
             }
-            return allowedDynamicProperties;
         }
-
-        /**
-         * @private
-         *
-         * @return void
-         */
-        function _addPropertyValidators() {
-
-
-            getFormEditorApp().addPropertyValidationValidator('isDynamicRecipientEmailField', function (formElement, propertyPath) {
-                var values = getDynamicRecipientAliasValues();
-                var valid = values.some(function (item) {
-                    var value = formElement.get(propertyPath);
-                    return value === '{' + item + '.email' + '}';
-                });
-
-                if (valid === false) {
-                    return 'invalid value';
-                }
-            });
-            getFormEditorApp().addPropertyValidationValidator('isDynamicRecipientLabelField', function (formElement, propertyPath) {
-                var values = getDynamicRecipientAliasValues();
-                var valid = values.some(function (item) {
-                    var value = formElement.get(propertyPath);
-                    return value === '{' + item + '.label' + '}';
-                });
-
-                if (valid === false) {
-                    return 'invalid value';
-                }
-            });
-        }
-
-        /**
-         * @public
-         *
-         * @param object formEditorApp
-         * @return void
-         */
-        function bootstrap(formEditorApp) {
-            _formEditorApp = formEditorApp;
-            _subscribeEvents();
-            _addPropertyValidators();
-        }
-
-        /**
-         * Publish the public methods.
-         * Implements the "Revealing Module Pattern".
-         */
-        return {
-            bootstrap: bootstrap
-        };
-
-    })($, StageComponent);
-});
+    );
+}
