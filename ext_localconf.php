1a273b415f91e8190b344ea5f92db248e17d3d3b
Andreas Kießling <kontakt@kiessling.tech>
2026-01-12 10:11:04 +0100

    Provide Site Set, split yaml loading
    Site Set based setups do not autoload the ext_typoscript_setup.typoscript file.
    https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/FileStructure/ExtTyposcriptSetupTyposcript.html
    
    Thus the backend / module config is moved to ext_localconf.php to have it loaded always
    while the frontend configuration is now either loaded through an own configuration file
    that is either automatically loaded via ext_typoscript_setup.typoscript or a site set.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,19 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(static function () {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterSubmit'][1517255631] = \AndreasKiessling\FormDynamicRecipient\Hooks\FormElementsOnSubmitHooks::class;
+
+    ExtensionManagementUtility::addTypoScriptSetup('
+module.tx_form {
+    settings {
+        yamlConfigurations {
+            1517255631 = EXT:form_dynamic_recipient/Configuration/Yaml/FormSetup.yaml
+        }
+    }
+}
+');
 });
